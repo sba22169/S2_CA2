fbb10a222611d73babc1be2f21b796d22285100a
Ambrose <sba22169@student.cct.ie>
2023-05-09 18:13:07 +0100

    Update Script.sh
    Added delays to bash script to allow time for commands to execute and debug for python not being called correctly. PATH.

--- Script.sh
+++ Script.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
 # Script to extract tweets and save to hdfs for spark processing
-./Twit_load.py
+
+python3 Twit_load.py
+sleep 8m
 jps
 start-dfs.sh
+sleep 8s
+
 start-yarn.sh
+sleep 8s
+
 jps
+sleep 20s
 hadoop fs -mkdir /sparkuser
-hadoop fs -put ./Tweets_*.* /sparkuser
+hadoop fs -put ./CA2_TWEETS_*.* /sparkuser
 hadoop fs -ls /sparkuser/
